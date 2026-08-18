7a2981015086dd4cc9d3e1566766b8bbe131adbf
Lucas Vinicius <lucassousarbr@gmail.com>
2026-08-18 18:49:31 -0300

    chore(ios): buildNumber 4 (icone novo + correcoes) para primeira versao na App Store

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.conciliaai.mobile",
-      buildNumber: "3",
+      buildNumber: "4",
       infoPlist: {
         NSFaceIDUsageDescription:
           "O ConciliaAI usa Face ID para proteger suas informações financeiras.",
